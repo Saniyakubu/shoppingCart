e4afd0436942f6620e4efcfa239e404777f89815
sani <ysani095@gmail.com>
2023-08-02 10:42:16 +0800

    updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,11 @@ const About = () => {
   return (
     <div className="about">
       <h1>
-        Welcome to Shopzy, your one-stop shop for all your favorites! Explore
+        Welcome to CartWise, your one-stop shop for all your favorites! Explore
         our vast selection of fashion, gadgets, home essentials, and more. Enjoy
         a seamless shopping experience with secure transactions and personalized
         recommendations. Join our vibrant community and discover a world of
-        endless possibilities. Happy shopping at Shopzy!
+        endless possibilities. Happy shopping at CartWise!
       </h1>
     </div>
   );
